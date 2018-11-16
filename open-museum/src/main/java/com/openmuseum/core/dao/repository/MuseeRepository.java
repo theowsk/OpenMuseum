9c1d5f520ec34620e0f6d5ec6e0d8c235f703ae2
DEBIERNE-RUSAK Nicolas <nicolasdebierne@outlook.com>
2018-11-16 16:46:35 +0100

    Initialisation JPA Oeuvre

--- open-museum/src/main/java/com/openmuseum/core/dao/repository/MuseeRepository.java
+++ open-museum/src/main/java/com/openmuseum/core/dao/repository/MuseeRepository.java
@@ -1,8 +1,11 @@
 package com.openmuseum.core.dao.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.openmuseum.core.models.Musee;
 
+@Repository
 public interface MuseeRepository extends JpaRepository<Musee, Long> {
 	// Requêtes complexes
 }
